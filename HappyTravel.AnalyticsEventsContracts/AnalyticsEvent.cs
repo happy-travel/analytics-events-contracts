d799e3dd096690d60e787837ae4583543e351078
Denis Nikolaev <dfs.str@gmail.com>
2022-10-24 15:58:04 +0500

    initial commit

---
+++ HappyTravel.AnalyticsEventsContracts/AnalyticsEvent.cs
@@ -0,0 +1,64 @@
+namespace HappyTravel.AnalyticsEventsContracts;
+
+public class AnalyticsEvent
+{
+    /// <summary>
+    /// Event datetime
+    /// </summary>
+    public DateTimeOffset DateTime { get; set; }
+    
+    /// <summary>
+    /// Event id
+    /// </summary>
+    public int EventId { get; set; }
+    
+    /// <summary>
+    /// Agency id
+    /// </summary>
+    public int AgencyId { get; set; }
+    
+    /// <summary>
+    ///  Agent id
+    /// </summary>
+    public int AgentId { get; set; }
+    
+    /// <summary>
+    /// Country name
+    /// </summary>
+    public string? Country { get; set; }
+    
+    /// <summary>
+    /// Locality name
+    /// </summary>
+    public string? Locality { get; set; }
+    
+    /// <summary>
+    /// Accommodation name
+    /// </summary>
+    public string? Accommodation { get; set; }
+    
+    /// <summary>
+    /// Supplier code
+    /// </summary>
+    public string? SupplierCode { get; set; }
+    
+    /// <summary>
+    /// Total price
+    /// </summary>
+    public decimal? TotalPrice { get; set; }
+    
+    /// <summary>
+    ///  Geo point
+    /// </summary>
+    public GeoPoint? GeoPoint { get; set; }
+
+    /// <summary>
+    /// Agency name
+    /// </summary>
+    public string AgencyName { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Agent name
+    /// </summary>
+    public string AgentName { get; set; } = string.Empty;
+}
---
+++ HappyTravel.AnalyticsEventsContracts/GeoPoint.cs
@@ -0,0 +1,14 @@
+namespace HappyTravel.AnalyticsEventsContracts;
+
+public class GeoPoint
+{
+    /// <summary>
+    /// Latitude
+    /// </summary>
+    public double Latitude { get; set; }
+
+    /// <summary>
+    /// Longitude
+    /// </summary>
+    public double Longitude { get; set; }
+}
